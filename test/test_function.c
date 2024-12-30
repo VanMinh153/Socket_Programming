292d789baca1539234862ed088ef6f2c76c8634f
nxd2409 <nxd2409@gmail.com>
2024-12-30 21:58:35 +0700

    add test

--- test/test_function.c
+++ test/test_function.c
@@ -5,14 +5,6 @@
 #include <unistd.h>
 
 int main() {
-  // char str[] = "   123   456";
-  char str[] = "   abc   def";
-  int int_arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  void *ptr = int_arr;
-  // ~ char *ptr = (char*) int_arr;
-  ptr = ptr + 3;
-  for (int i = 0; i < 9; i++) {
-    printf("%d ", *((int*) ptr + i));
-  }  
+  // test
   return 0;
 }
